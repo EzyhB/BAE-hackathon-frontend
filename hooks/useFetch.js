a0c70c0f5d6e087aeb7d23a36db15c973591b70c
drinobre <dri_nobre@hotmail.com>
2022-03-28 11:06:10 +0100

    use fetch

---
+++ hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useSate("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+
+    async function fetchAPI() {
+      try {
+        const response = await fetch(`${url}`);
+        const dataResponse = await response.json();
+        setData(dataResponse);
+        setError(null);
+
+        console.log(dataResponse);
+        console.log(data);
+      } catch (error) {
+        setError(error);
+        setData(null);
+        console.log("Error Fetching");
+      }
+    }
+
+    fetchAPI();
+  }, [url]);
+
+  return { data, error };
+}
