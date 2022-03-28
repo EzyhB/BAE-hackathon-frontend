a2d1fb2d3d6b8ed09c1c42a3c4ee0c7cd6eca03e
drinobre <dri_nobre@hotmail.com>
2022-03-28 12:00:57 +0100

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import { Container, Grid } from "@mui/material";
 import DisplayCards from "../components/DisplayCards";
+import useFetch from "../hooks/useFetch.js";
+
+const { API_ID } = process.env;
 
 export default function Home() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?zip=DA16,GB&appid=${API_ID}`;
+
+  const { data, error } = useFetch(url);
+  console.log(API_ID);
+
   return (
     <Container>
       <Container>
