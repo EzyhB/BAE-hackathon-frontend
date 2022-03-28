55f51dad0f7a67500a97614b5723135cd4d2fd6d
EzyhB <ezyhbmail@gmail.com>
2022-03-28 11:25:11 +0100

    display cards done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import { Container, Grid } from "@mui/material";
+import DisplayCards from "../components/DisplayCards";
 
 export default function Home() {
   return (
     <Container>
       <Container>
-        <Grid container></Grid>
+        <Grid container>
+          <Grid item>
+            <DisplayCards />
+          </Grid>
+        </Grid>
       </Container>
     </Container>
   );
