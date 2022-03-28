73892de3f00143b785f1050671e58a0cb9bb4359
EzyhB <ezyhbmail@gmail.com>
2022-03-28 12:25:56 +0100

    added error catch

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import { Container, Grid } from "@mui/material";
 import DisplayCards from "../components/DisplayCards";
 import useFetch from "../hooks/useFetch.js";
 
-const { API_ID } = process.env;
-
 export default function Home() {
   const url = `https://api.openweathermap.org/data/2.5/weather?zip=DA16,GB&appid=fed7a956d8b8ed2124f69e9691839d44`;
 
   const { data, error } = useFetch(url);
-  console.log("DATA STUFF", data);
+
+  if (error) {
+    return <div>contact tEcHnIkAl sPoArT...</div>;
+  }
 
   return (
     <Container>
